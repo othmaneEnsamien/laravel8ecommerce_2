9c626d07087de8c32d6ef847a78b288a4644c9c4
othmaneEnsamien <othmane.elkhaddar@gmail.com>
2022-04-08 22:14:27 +0200

    first commit

---
+++ resources/views/auth/reset-password.blade.php
@@ -0,0 +1,82 @@
+<x-base-layout>
+   {{-- <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
+
+        <x-jet-validation-errors class="mb-4" />
+
+        <form method="POST" action="{{ route('password.update') }}">
+            @csrf
+
+            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+            <div class="block">
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+            </div>
+
+            <div class="mt-4">
+                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <x-jet-button>
+                    {{ __('Reset Password') }}
+                </x-jet-button>
+            </div>
+        </form>
+    </x-jet-authentication-card>  --}}
+    <main id="main" class="main-site left-sidebar">
+
+		<div class="container">
+
+			<div class="wrap-breadcrumb">
+				<ul>
+					<li class="item-link"><a href="/" class="link">home</a></li>
+					<li class="item-link"><span>Reset password</span></li>
+				</ul>
+			</div>
+			<div class="row">
+				<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 col-md-offset-3">							
+					<div class=" main-content-area">
+						<div class="wrap-login-item ">
+							<div class="register-form form-item ">
+                                <x-jet-validation-errors class="mb-4" />
+								<form class="form-stl" action="{{ route('password.update') }}" name="frm-login" method="POST" >
+                                    @csrf
+                                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+									<fieldset class="wrap-input">
+										<label for="frm-reg-email" value="{{ __('Email') }}">Email Address*</label>
+										<input type="email" id="frm-reg-email" name="email" placeholder="Email address" :value="old('email', $request->email)" required autofocus >
+									</fieldset>
+									
+					
+									<fieldset class="wrap-input item-width-in-half left-item ">
+										<label for="frm-reg-pass" value="{{ __('Password') }}">Password *</label>
+										<input type="password" id="frm-reg-pass" name="password" placeholder="Password" required autocomplete="new-password">
+									</fieldset>
+									<fieldset class="wrap-input item-width-in-half ">
+										<label for="frm-reg-cfpass" value="{{ __('Confirm Password') }}">Confirm Password *</label>
+										<input type="password" id="frm-reg-cfpass" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">
+									</fieldset>
+                                    
+                                    <input type="submit" class="btn btn-sign" value=" {{ __('Reset Password') }}" >
+								</form>
+							</div>											
+						</div>
+					</div><!--end main products area-->		
+				</div>
+			</div><!--end row-->
+
+		</div><!--end container-->
+
+	</main> 
+	<!--main area-->
+</x-base-layout>
